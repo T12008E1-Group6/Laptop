4773be75759f2bbb434c6f840564ad9b750079ec
T12008E1-Group6 <85565834+T12008E1-Group6@users.noreply.github.com>
2021-06-24 11:19:32 +0700

    Add files via upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.appIndex')
+
+@section('content')
+    <div class="row py-4" style="display:flex">
+        <div class="col-md-12 shadow-md">
+            <img src="images/AboutUs.gif" height="500vh" alt="">
+        </div>
+
+    </div>
+<div class="" style="display:flex; ">
+    <div class="col-md-12 py-4" style="font-size:1.2rem; text-indent: 1.5rem; padding: 5% 5% 0%">
+        <p>
+            Welcome to Dvice, your number one source for all things tech. We're dedicated to giving you the very best of Laptops,
+             with a focus on customer satisfaction, professionalism, and community.
+        </p>
+        <br>
+        <p>
+            Founded in 2021 by Group 6, Dvice has come a long way from its beginnings in Ho Chi Minh. When we first started out, 
+            our passion for "devices for all" drove them to take this big leap of faith, so that Dvice can offer you an opportunity
+            to own a device that's catered to you, enabling our community to excel in their study, career and/or life. We now serve
+            customers all over Vietnam and are thrilled that we're able to turn our passion into our own website. 
+        </p>
+        <br>
+        <p>
+            We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments,
+             please don't hesitate to contact us.
+        </p>
+        <br>
+        <p>
+            Sincerely,
+        <br>
+            <i style="font-size:1.5rem">Dvice shop</i>
+        </p>    
+    </div>
+</div>
+@endsection
